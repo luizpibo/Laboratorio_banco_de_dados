7c92101a25baee70e310c61832735bc7bcca9e2c
Alex Vieira Lopes <86755313+zakale@users.noreply.github.com>
2021-10-27 00:25:08 -0300

    push select.sql

---
+++ select.sql
@@ -0,0 +1,17 @@
+SELECT provas.id, provas.nota, professores.id_funcionario, professores.id_usuario, professores.id_pessoa, professores.nome, alunos.id_funcionario, alunos.id_usuario, alunos.id_pessoa, alunos.nome
+FROM provas INNER JOIN professores ON provas.id_professor = professores.id
+INNER JOIN alunos ON provas.id_aluno = alunos.id;
+
+SELECT apresentacoes.id, apresentacoes.data_apresentacao, professores.id, alunos.id 
+FROM apresentacoes 
+INNER JOIN professores ON apresentacoes.id_professores = professores.id 
+INNER JOIN alunos ON apresentacoes.id_alunos = alunos.id;
+
+SELECT * FROM instrumentos INNER JOIN alunos ON instrumentos.id_alunos = alunos.id;
+
+SELECT * FROM alunos INNER JOIN intrumentnos ON alunos.id_instrumentos = intrumentnos.id;
+
+
+ 
+ 
+ 
